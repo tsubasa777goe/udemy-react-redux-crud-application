143050a3c4565a9ab59745779030615c9e127c91
tsubasa777goe <tsubasa.ggg.7777.spjack.goe@gmail.com>
2021-11-29 21:55:29 +0900

    Learn state and setState

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
 
-function App() {
-  // const greeting = "Hiiiii, TOM!!!"
-  // const dom = <h1 className="foo">{greeting}</h1>
+const App = () =>(
+    <Counter></Counter>
+)
 
-const profiles = [
-  { name: "Taro", age: 20 },
-  { name: "Hanako", age: 5 },
-  { name: "NoName", age: 3}
-]
+class Counter extends Component {
+  constructor(props){
+    super(props)
+    console.log(this.state)
+    this.state = { count: 0 }
+  }
 
-  return (
-    // <input type="text"  onClick={() => {console.log("I am a cat")}}/>
-    // <input type="text"  onChange={() => {console.log("I am a cat")}}/>
-    <div>
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index} />
-        })
-      }
-      <User name={"Taro"} age={10} />
-      <User name={"Hanako"} age={5} />
-    </div>
-);
+  handlePlusButton = () => {
+    console.log("handlePlusButton")
+    console.log(this.state.count)
+    this.setState({ count: this.state.count + 1})
+  }
+
+  handleMinusButton = () => {
+    console.log("handleMinusButton")
+    console.log(this.state.count)
+    this.setState({ count: this.state.count - 1})
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>+1</button>
+      </React.Fragment>
+      )
+  }
+
 }
 
-const User = (props) => {
-  return <div>Hi, I am {props.name}! , and {props.age}years old!</div>
-}
-
-User.propTypes = {
-  name: PropTypes.string,
-  // string:文字列　それ以外を入力するとエラーを出力
-  age: PropTypes.number.isRequired
-  // number:数字　それ以外を入力するとエラーを出力
-  // isRequired:入力必須　入力されていなければエラー
-}
 
 export default App;
